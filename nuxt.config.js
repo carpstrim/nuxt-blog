86e239f0260922d6cc11f1577e30efdbef30b5bc
Kohei Yasumi <k.yasumi@osakagas-developers.com>
2020-02-01 01:55:53 +0900

    added meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ require("dotenv").config() //一番上に
 const client = require("./plugins/contentful")
 
 import colors from 'vuetify/es5/util/colors'
-import topImg from "@/static/top.jpg"
 
 
 export default {
@@ -16,7 +15,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'og:image', property: 'og:image', content: topImg },
+      { hid: 'og:image', property: 'og:image', content: '/top.jpg' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
