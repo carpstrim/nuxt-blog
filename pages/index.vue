cecc32fc1ea5b325559059bb0f3ee2888c9a9851
Kohei Yasumi <k.yasumi@osakagas-developers.com>
2020-03-12 16:43:03 +0900

    ads arennging

--- pages/index.vue
+++ pages/index.vue
@@ -16,9 +16,7 @@
         <article>
           <category-list :categories="categories" class="mt-10 mb-10" style="margin: 0 25px" />
         </article>
-        <div style="text-align:center">
-          <adsbygoogle ad-slot="7918916412" style="width: 300px" />
-        </div>
+        <adsbygoogle ad-slot="7918916412" style="width: 300px; margin: 0 auto" />
       </v-flex>
     </v-layout>
   </v-container>
