48b352bb4689c7f9bd0ac433dc797d18decd085c
Kohei Yasumi <k.yasumi.jug@gmail.com>
2020-04-05 02:20:09 +0900

    re-displayed ads in posts

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -20,11 +20,7 @@
               <v-img　 class="ma-3" :src="post.image.fields.file.url" aspect-ratio="1.77" />
               <div class="content" v-html="$md.render(post.content)"></div>
               <div style="text-align:center; margin: 0 0 15px 0">
-                <!--<adsbygoogle
-                  ad-slot="7918916412"
-                  ad-format
-                  style="width: 300px; height: 120px; margin: 0 auto"
-                />-->
+                <adsbygoogle ad-slot="7918916412" />
               </div>
             </v-container>
           </article>
